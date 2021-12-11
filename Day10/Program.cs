0210073555ad392acdeb66bfbdd603b084dd6362
Peter Baumhart <peter.baumhart@gmail.com>
2021-12-11 12:49:52 -0600

    Day 10 and 11, rename folders/projects

---
+++ Day10/Program.cs
@@ -0,0 +1,107 @@
+﻿var open  = "([{<";
+var close = ")]}>";
+var stack = new Stack<char>();
+
+//Taking each part separately
+
+//Part 1 - Corrupted Lines
+
+//map the character to the score it receives
+var corruptScores = new Dictionary<char, int>()
+{
+    {')', 3 },
+    {']', 57 },
+    {'}', 1197 },
+    {'>', 25137 }
+};
+
+//Simple stack based solution. Read in each character of the line. If it is an opening character, push it onto the stack
+//If is a closing character, pop the stack if it has anything on it and check to make sure the closing character
+//matches the opening character that was on the stack. If it doesn't, or if the stack was empty, we have a corrupted line
+//so calculate the score and return;
+var syntaxErrorScore = File.ReadLines("input.txt").Sum(line =>
+{
+    stack.Clear();
+    var index = 0;
+    do
+    {
+        var c = line[index];
+        if (open.Contains(c))
+        {
+            stack.Push(c);
+        }
+        else if (!stack.Any()) //stack is empty but we encountered a close character
+        {
+            return corruptScores[c];
+        }
+        else
+        {
+            var p = stack.Pop();
+            if (open.IndexOf(p) != close.IndexOf(c))
+            {
+                return corruptScores[c];
+            }
+        }
+        index++;
+    } while (index < line.Length);
+    return 0;
+});
+
+Console.WriteLine($"Syntax Error Score: {syntaxErrorScore}");
+
+//Part 2 - Incomplete Lines
+//Do the same thing as part 1, just ignore the corrupted lines (return score of 0)
+//If we get to the end without any corruption, we can look at the stack to see what's left to close out the line
+//and calculate the score. This time though, because of how scoring works, instead of summing the results, enumerate a list of scores
+//and find the median (guaranteed odd number of scores)
+
+//we can just look at what's in the stack when a line is done, which is all open characters, to calculate the score
+var incompleteScores = new Dictionary<char, int>()
+{
+    {'(', 1 },
+    {'[', 2 },
+    {'{', 3 },
+    {'<', 4 }
+};
+
+var autoCompleteScores = File.ReadLines("input.txt").Select(line =>
+{
+    stack.Clear();
+    var index = 0;
+    do
+    {
+        var c = line[index];
+        if (open.Contains(c))
+        {
+            stack.Push(c);
+        }
+        else if (!stack.Any())
+        {
+            return 0; //corrupt
+        }
+        else
+        {
+            var p = stack.Pop();
+            if (open.IndexOf(p) != close.IndexOf(c))
+            {
+                return 0; //corrupt
+            }
+        }
+        index++;
+    } while (index < line.Length);
+
+    //calculate the score. The numbers get big, so use int64
+    //elements are enumerated based on how they would come out if popped, so aggregate on the stack itself
+    if (stack.Any())
+    {
+        return stack.Aggregate(0L, (a, c) => 5 * a + incompleteScores[c]);
+    }
+    else
+    {
+        return 0; //should never get here because all lines are either corrupt or incomplete
+    }
+}).Where(score => score > 0).OrderBy(score => score).ToArray();  
+//scores of 0 were corrupted lines, so ignore. Need to stamp out the array to actually calculate the scores
+
+Console.WriteLine($"Autocomplete Score: {autoCompleteScores[autoCompleteScores.Length / 2]}");
+
---
+++ Day11/Program.cs
@@ -0,0 +1,107 @@
+﻿var input = File.ReadLines("input.txt").Select(line => line.Select(c => (energy: (int)char.GetNumericValue(c), flashed: false)).ToArray()).ToArray();
+
+var stepFlashes = 0;
+var totalFlashes = 0;
+var flashes = 0;
+var steps = 0;
+
+// Follow the algorithm as described by the problem. Each iteration of the outer while loop is one step
+// Each step
+//   1) increase the energy of the octopi by 1
+//   2) see which ones flash, which causes neighbors energy to increase by 1, and keep going until there are no more flashes
+//   3) reset the energy level to 0 for any that flashed
+//
+// Part 2 is where the problem lies. This solution goes through the grid calling the flash function on each octopus.
+// The flash function then recursively checks neighbors to see if they should then flash and so on.
+while(true)
+{
+    stepFlashes = 0;
+    flashes = 0;
+
+    //increase energy
+    input.ForEach2D((i, j, octopus) => input[i][j].energy++);
+    
+    input.ForEach2D((i,j,octopus) =>
+    {
+        flashes += Flash(i, j, input, 0);
+    });
+
+    totalFlashes += flashes;
+    stepFlashes += flashes;
+
+    input.ForEach2D((i, j, octopus) => {
+        input[i][j].flashed = false;
+        if (input[i][j].energy > 9)
+            input[i][j].energy = 0;
+    });
+
+    steps++;
+
+    //part 1 - how many flashes after 100 steps
+    if(steps == 100)
+        Console.WriteLine($"Flashes after 100 steps: {totalFlashes}");
+
+    //part 2 - how many steps until all of the octopi flash in the same step
+    //there are 100 octopi, so check if there were 100 flashes in the step
+    if (stepFlashes == 100)
+    {
+        Console.WriteLine($"Steps until all flash in one step {steps}");
+        break;
+    }
+}
+
+//Recursively check neighbors if the current octopus flashed
+//incomingEnergy parameter is to differentiate between the top level calls and the recursive calls
+//top level calls are not due to a flash, so we don't increase the energy first
+//but recursive calls are due to a flash.
+static int Flash(int i, int j, (int energy, bool flashed)[][] input, int incomingEnergy = 1)
+{
+    int h = input.Length;
+    int w = input[i].Length;
+
+    var flashes = 0;
+
+    
+    input[i][j].energy += incomingEnergy;
+
+    if (input[i][j].energy > 9 && !input[i][j].flashed)
+    {
+        flashes++;
+        input[i][j].flashed = true; 
+
+        if (i > 0)
+            flashes += Flash(i - 1, j, input);
+        if (j > 0)
+            flashes += Flash(i, j - 1, input);
+        if (i > 0 && j > 0)
+            flashes += Flash(i - 1, j - 1, input);
+        if (i < h - 1)
+            flashes += Flash(i + 1, j, input);
+        if (j < w - 1)
+            flashes += Flash(i, j + 1, input);
+        if (i < h - 1 && j < w - 1)
+            flashes += Flash(i + 1, j + 1, input);
+        if (i > 0 && j < w - 1)
+            flashes += Flash(i - 1, j + 1, input);
+        if (i < h - 1 && j > 0)
+            flashes += Flash(i + 1, j - 1, input);
+    }
+
+    return flashes;
+        
+}
+
+static class Extensions
+{
+    public static void ForEach2D<T>(this T[][] ts, Action<int, int, T> action)
+    {
+        for (var i = 0; i < ts.Length; i++)
+        {
+            for (var j = 0; j < ts[i].Length; j++)
+            {
+                action(i,j,ts[i][j]);
+            }
+        }
+    }
+}
+
