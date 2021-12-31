504b05ac52f72733248b9c91dcbd1d49431280b1
Peter Baumhart <peter.baumhart@gmail.com>
2021-12-30 23:34:25 -0600

    Day 19 complete.

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,21 +1,22 @@
-﻿/* WORK IN PROGRESS, DOES NOT WORK */
-/* WORK IN PROGRESS, DOES NOT WORK */
-/* WORK IN PROGRESS, DOES NOT WORK */
+﻿using System.Text.RegularExpressions;
 
-using System.Text.RegularExpressions;
+/* Day 19
+ * Got stuck on this one when my solution worked for sample, but not for the input. Made a dumb mistake
+ * and wasn't inverting the rotation matrix when necessary, only the translation. This is also one 
+ * where i end up second guessing what i'm doing because it seems too complex.
+ */
 
-var input = File.ReadAllLines("sample.txt");
+var input = File.ReadAllLines("input.txt");
 
 var scanners = new List<Scanner>();
 
-
-for(var i = 0; i < input.Length; i++)
+for (var i = 0; i < input.Length; i++)
 {
     var scanner = new Scanner();
     scanner.Id = int.Parse(Regex.Match(input[i], "\\d+").Value);
-    while(true && ++i < input.Length)
-    { 
-        if(string.IsNullOrEmpty(input[i]))
+    while (true && ++i < input.Length)
+    {
+        if (string.IsNullOrEmpty(input[i]))
         {
             break;
         }
@@ -25,181 +26,315 @@ for(var i = 0; i < input.Length; i++)
     scanners.Add(scanner);
 }
 
-foreach(var scanner in scanners)
+//To match overlapping scanners, compute each beacons distance to each other beacon in the scanners beacon set
+//Because scanners overlap by 12 or more beacons, and the distances between those beacons is independent of coordinate system
+//we can check to see which beacons have intersecting distance lists
+foreach (var scanner in scanners)
 {
-    foreach(var beacon in scanner.Beacons)
+    foreach (var beacon in scanner.Beacons)
     {
-        var d2s = new List<int>();
-        foreach(var b in scanner.Beacons)
+        var distancesSquared = new List<int>();
+        foreach (var b in scanner.Beacons)
         {
-            if(b != beacon)
+            if (b != beacon)
             {
-                var dx = b.x - beacon.x; 
+                var dx = b.x - beacon.x;
                 var dy = b.y - beacon.y;
                 var dz = b.z - beacon.z;
-                d2s.Add(dx * dx + dy * dy + dz * dz);
+                distancesSquared.Add(dx * dx + dy * dy + dz * dz);
             }
         }
-        scanner.Distances2.Add(beacon, d2s);
+        scanner.DistancesSquared.Add(beacon, distancesSquared);
     }
 }
 
+//just used for debugging purposes, a list of all the overlapping scanners and which beacons overlap
 var overlaps = new Dictionary<(int i, int j), List<((int xi, int yi, int zi), (int xj, int yj, int zj))>>();
-var transforms = new Dictionary<(int i, int j), int[,]>();
-for(var i = 0; i < scanners.Count() - 1;i++)
+
+//when 2 scanners overlap, we compute a transform to map the coordinates in j to coordinates in i and store here
+var transforms = new Dictionary<(int i, int j), transform>();
+
+//compare each scanner with each other scanner, looking for overlaps
+for (var i = 0; i < scanners.Count() - 1; i++)
 {
     for (var j = i + 1; j < scanners.Count(); j++)
     {
-        var os = scanners[i].Distances2
-            .Select(id2 => (id2.Key, scanners[j].Distances2
-                .Where(jd2 => jd2.Value.Intersect(id2.Value).Count() >= 11)
-                .Select(jd22 => jd22.Key).FirstOrDefault()))
-            .Where(overlap => overlap.Item2 != (0, 0, 0)).ToList();
-        if(os.Count() > 0)
+        //we can tell if 2 beacons from different scanners are the same if we compare their distances to other beacons
+        //if 2 beacons have 11+ distances to other beacons in ther scanner set that are the same, then the beacons are the same
+        //and the 2 scanners overlap
+        var os = scanners[i].DistancesSquared
+            .Select(iScannerDistances2 => (beacon1: iScannerDistances2.Key, beacon2: scanners[j].DistancesSquared //select the beacon from i and any beacon from j
+                .Where(jScannerDistances2 => jScannerDistances2.Value.Intersect(iScannerDistances2.Value).Count() >= 11) //where they have 11 or more distances in common
+                .Select(jScannerDistances2 => jScannerDistances2.Key).FirstOrDefault()))
+            .Where(overlap => overlap.beacon2 != (0, 0, 0)).ToList(); //get rid of the defaults, there has to be a better way to do this
+
+        if (os.Count() > 0)
         {
             overlaps.Add((i, j), os);
-            (int dx, int dy, int dz) ds1 = default, ds2 = default;
-            for(var mz = 0; mz < os.Count()-1; mz++)
+
+            //to find the relative orientation, calculate a vector between the first and second beacons in each coordinate system
+            //since the systems are only rotated in 90 degree on axis intervals and translated, the components of the vectors
+            //should have equal magnitudes, just swizzled, so find out which components are which and construct a rotation matrix
+            var ds1 = (dx: os[0].beacon2.x - os[1].beacon2.x, dy: os[0].beacon2.y - os[1].beacon2.y, dz: os[0].beacon2.z - os[1].beacon2.z);
+            var ds2 = (dx: os[0].beacon1.x - os[1].beacon1.x, dy: os[0].beacon1.y - os[1].beacon1.y, dz: os[0].beacon1.z - os[1].beacon1.z);
+
+            var roationMatrix = new int[3, 3];
+            //for each axis, check which axis it ends up rotating to 
+
+            //x axis
+            if (Math.Abs(ds2.dx) == Math.Abs(ds1.dy))
             {
-                var t = new int[3, 4];
-                ds1 = (os[mz].Item2.x - os[mz + 1].Item2.x, os[mz].Item2.y - os[mz + 1].Item2.y, os[mz].Item2.z - os[mz + 1].Item2.z);
-                ds2 = (os[mz].Key.x - os[mz + 1].Key.x, os[mz].Key.y - os[mz + 1].Key.y, os[mz].Key.z - os[mz + 1].Key.z);
-                //if(transforms.Any(t => t.Key.i == j))
-                //{
-                //    var temp = ds1;
-                //    ds1 = ds2;
-                //    ds2 = temp;
-                //}
-                //if (ds1.dx != ds1.dy && ds1.dy != ds1.dz && ds1.dz != ds1.dx)
-                //break;
-
-                //x transforms
-                if (Math.Abs(ds2.dx) == Math.Abs(ds1.dy))
-                {
-                    var tval = ds2.dx / ds1.dy;
-                    t[0, 1] = tval;
-                }
-                else if (Math.Abs(ds2.dx) == Math.Abs(ds1.dz))
-                {
-                    var tval = ds2.dx / ds1.dz;
-                    t[0, 2] = tval;
-                }
-                else
-                {
-                    t[0, 0] = ds2.dx / ds1.dx;
-                }
-
-                //y transforms
-                if (Math.Abs(ds2.dy) == Math.Abs(ds1.dx))
-                {
-                    var tval = ds2.dy / ds1.dx;
-                    t[1, 0] = tval;
-                }
-                else if (Math.Abs(ds2.dy) == Math.Abs(ds1.dz))
-                {
-                    var tval = ds2.dy / ds1.dz;
-                    t[1, 2] = tval;
-                }
-                else
-                {
-                    t[1, 1] = ds2.dy / ds1.dy;
-                }
-
-                //z transforms
-                if (Math.Abs(ds2.dz) == Math.Abs(ds1.dx))
-                {
-                    var tval = ds2.dz / ds1.dx;
-                    t[2, 0] = tval;
-                }
-                else if (Math.Abs(ds2.dz) == Math.Abs(ds1.dy))
-                {
-                    var tval = ds2.dz / ds1.dy;
-                    t[2, 1] = tval;
-                }
-                else
-                {
-                    t[2, 2] = ds2.dz / ds1.dz;
-                }
-
-
-                var t1 = m.matmul(t, os[mz].Item2);
-                t[0, 3] = os[mz].Key.x - t1.x;
-                t[1, 3] = os[mz].Key.y - t1.y;
-                t[2, 3] = os[mz].Key.z - t1.z;
-
-                if (transforms.Any(t => t.Key.i == j))
-                {
-                    //transforms.Add((i, j), itz);
-                }
-                else
-                {
-                    //transforms.Add((j, i), t);
-                }
+                var tval = ds2.dx / ds1.dy;
+                roationMatrix[0, 1] = tval;
+            }
+            else if (Math.Abs(ds2.dx) == Math.Abs(ds1.dz))
+            {
+                var tval = ds2.dx / ds1.dz;
+                roationMatrix[0, 2] = tval;
+            }
+            else
+            {
+                roationMatrix[0, 0] = ds2.dx / ds1.dx;
             }
 
-            
+            //y axis
+            if (Math.Abs(ds2.dy) == Math.Abs(ds1.dx))
+            {
+                var tval = ds2.dy / ds1.dx;
+                roationMatrix[1, 0] = tval;
+            }
+            else if (Math.Abs(ds2.dy) == Math.Abs(ds1.dz))
+            {
+                var tval = ds2.dy / ds1.dz;
+                roationMatrix[1, 2] = tval;
+            }
+            else
+            {
+                roationMatrix[1, 1] = ds2.dy / ds1.dy;
+            }
+
+            //z axis
+            if (Math.Abs(ds2.dz) == Math.Abs(ds1.dx))
+            {
+                var tval = ds2.dz / ds1.dx;
+                roationMatrix[2, 0] = tval;
+            }
+            else if (Math.Abs(ds2.dz) == Math.Abs(ds1.dy))
+            {
+                var tval = ds2.dz / ds1.dy;
+                roationMatrix[2, 1] = tval;
+            }
+            else
+            {
+                roationMatrix[2, 2] = ds2.dz / ds1.dz;
+            }
+
+            //to find how to translate the origin, we can now rotate a beacon in one coordinate system and find the vector from there to
+            //the same beacon in the other coordinate system
+            var rotatedBeacon2 = m.matmul(roationMatrix, os[0].beacon2);
+            var translationVector = (os[0].beacon1.x - rotatedBeacon2.x, os[0].beacon1.y - rotatedBeacon2.y, os[0].beacon1.z - rotatedBeacon2.z);
+
+            //store the transform as a rotation and translation
+            transforms.Add((j, i), new transform() { rotation = roationMatrix, translation = translationVector });
         }
     }
 }
 
+/* Part 1: Find the size of the beacon set
+ *   Once we have all the transformations from one set to another,
+ *   we can find tranformations from any coordinate system to scanner 0's coordinate system.
+ *   Once we can do that, just transform all the beacons to scanner 0's coordinate system and count the unique positions
+ * 
+ */
+
+//Construct the full set of beacons
 var beaconSet = new HashSet<(int x, int y, int z)>();
 
-foreach(var b in scanners.First().Beacons)
+//first, add scanner 0's beacons, as it will be our reference coordinate system
+foreach (var b in scanners.First().Beacons)
 {
     beaconSet.Add(b);
 }
 
+//for each other scanner, find a tranformation path between it and the reference scanner (0), then apply that transformation to each beacon
+//and add it to the set. The hashset will ignore duplicates
+//also, transform the origin for part 2
 foreach (var s in scanners.Skip(1))
 {
-    var currentScanner = 0;
-    var nextScanner = currentScanner;
-    int[,] t;
-    List<int> visited = new List<int>();
-    (int x, int y, int z) transformedBeacon = default;
+    var transformList = getTransform(s.Id);
+
+    s.TransformedPosition = transformBeacon((0, 0, 0), transformList);
+
     foreach (var b in s.Beacons)
     {
-        currentScanner = s.Id;
-        nextScanner = currentScanner;
-        visited.Clear();
-        transformedBeacon = b;
-        do
-        {
-            currentScanner = nextScanner;
-            var transform = transforms.Where(t => t.Key.i == currentScanner && !visited.Contains(t.Key.j)).FirstOrDefault();
-            t = transform.Value;
-            nextScanner = transform.Key.j;
-            visited.Add(transform.Key.i);
-            transformedBeacon = m.matmul(t, transformedBeacon);
-            if(currentScanner == s.Id)
-            {
-                s.TransformedBeacons.Add(transformedBeacon);
-            }
-        } while (nextScanner != 0);
+        var transformedBeacon = transformBeacon(b, transformList);
+        s.TransformedBeacons.Add(b, transformedBeacon);
         beaconSet.Add(transformedBeacon);
     }
 }
 
-var x = m.matmul(transforms[(1, 0)], overlaps.First().Value[0].Item2);
-var xy = m.matmul(transforms[(1, 0)], overlaps.Skip(1).First().Value[0].Item2);
 
-var xxy = m.matmul(transforms[(4, 1)], overlaps.Skip(2).First().Value[0].Item2);
-var zzz = 0;
+/* Part 2
+ *   Just go through all the scanners finding the maximum manhatten distance between any 2 scanners
+ */
+long maxDistance = long.MinValue;
 
-
-public static class m
+for(var i = 0; i < scanners.Count(); i++)
 {
-    public static (int x, int y, int z) matmul(int[,] t, (int x, int y, int z) p)
+    for(var j = i + 1; j < scanners.Count(); j++)
     {
-        return (t[0, 0] * p.x + t[0, 1] * p.y + t[0, 2] * p.z + t[0, 3],
-                t[1, 0] * p.x + t[1, 1] * p.y + t[1, 2] * p.z + t[1, 3],
-                t[2, 0] * p.x + t[2, 1] * p.y + t[2, 2] * p.z + t[2, 3]);
+        maxDistance = Math.Max(maxDistance, m.mdist(scanners[i].TransformedPosition, scanners[j].TransformedPosition));
+    }
+}
+
+Console.WriteLine($"Part 1: {beaconSet.Count()} beacons.");
+Console.WriteLine($"Part 2: {maxDistance} units between farthest apart scanners.");
+
+
+/*
+ *  This takes a list of transformations (and whether to invert them or not) and applies them in order to a beacon
+ * 
+ */
+(int x, int y, int z) transformBeacon((int x, int y, int z) beacon, List<(transform transform, bool invert, (int i, int j))> transformList)
+{
+    (int x, int y, int z) transformedBeacon = default;
+
+    transformedBeacon = beacon;
+    foreach (var t in transformList)
+    {
+        var (translation, rotation) = (t.transform.translation, t.transform.rotation);
+        if (t.invert)
+        {
+            //the inverse of the rotation is just the transpose
+            translation = (-translation.x, -translation.y, -translation.z);
+            rotation = m.transpose(rotation);
+            transformedBeacon = m.vadd(translation, transformedBeacon);
+            transformedBeacon = m.matmul(rotation, transformedBeacon);
+        }
+        else
+        {
+            transformedBeacon = m.matmul(rotation, transformedBeacon);
+            transformedBeacon = m.vadd(translation, transformedBeacon);
+        }
+    }
+    return transformedBeacon;
+}
+
+/* this finds the list of transforms that will take us from scannerId's coordinate system to scanner 0's coordinate system
+ * recursively looks through the list of transforms until it gets to 0. Nothing fancy, not trying to find the shortest path
+ * this feels off, but it's working
+ */
+List<(transform transform, bool invert, (int i, int j))> getTransform(int scannerId)
+{
+    Stack<(transform, bool, (int i, int j))> st = new Stack<(transform, bool, (int i, int j))>();
+    List<int> visited = new List<int>() { scannerId };
+
+    thelper(scannerId, st, visited);
+
+    return st.ToList();
+}
+
+bool thelper(int scannerId, Stack<(transform transform, bool invert, (int i, int j))> stack, List<int> visited)
+{
+    //first see if there's anyway to get to 0 from scannerId
+    //we need to keep track of the direction of the transform
+    if (transforms.ContainsKey((scannerId, 0)))
+    {
+        stack.Push((transforms[(scannerId, 0)], false, (scannerId, 0)));
+        return true;
+    }
+    else if (transforms.ContainsKey((0, scannerId)))
+    {
+        stack.Push((transforms[(scannerId, 0)], true, (0, scannerId)));
+        return true;
+    }
+    else
+    {
+        //if we don't have a path to 0 from here, search through all the transforms that either start at scannerId, or end at scannerId (which we will invert)
+        //that don't go/start somewhere we've already been
+
+        //first check starting at scannerId
+        var candidates = transforms.Where(t => t.Key.i == scannerId && !visited.Contains(t.Key.j));
+        foreach (var candidate in candidates)
+        {
+            visited.Add(candidate.Key.i);
+            var found = thelper(candidate.Key.j, stack, visited);
+            if (found)
+            {
+                stack.Push((candidate.Value, false, candidate.Key));
+                return true;
+            }
+        }
+
+        //If we are still here check ending at scannerId
+        candidates = transforms.Where(t => t.Key.j == scannerId && !visited.Contains(t.Key.i));
+        foreach (var candidate in candidates)
+        {
+            visited.Add(candidate.Key.j);
+            var found = thelper(candidate.Key.i, stack, visited);
+            if (found)
+            {
+                stack.Push((candidate.Value, true, candidate.Key));
+                return true;
+            }
+        }
+
+        //If we are still here we've hit a dead end
+        return false;
     }
 }
 
 class Scanner
 {
     public int Id;
+    public (int x, int y, int z) TransformedPosition = default;
     public HashSet<(int x, int y, int z)> Beacons = new HashSet<(int x, int y, int z)>();
-    public HashSet<(int x, int y, int z)> TransformedBeacons = new HashSet<(int x, int y, int z)>();
-    public Dictionary<(int x, int y, int z), List<int>> Distances2 = new Dictionary<(int x, int y, int z), List<int>>();
+    public Dictionary<(int x, int y, int z), (int x, int y, int z)> TransformedBeacons = new Dictionary<(int x, int y, int z), (int x, int y, int z)>();
+    public Dictionary<(int x, int y, int z), List<int>> DistancesSquared = new Dictionary<(int x, int y, int z), List<int>>();
+}
+
+//utilites
+class transform
+{
+    public (int x, int y, int z) translation;
+    public int[,] rotation;
+}
+
+public static class m
+{
+    public static (int x, int y, int z) vadd((int x, int y, int z) lhs, (int x, int y, int z) rhs)
+    {
+        return (lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+    }
+
+    public static (int x, int y, int z) vsub((int x, int y, int z) lhs, (int x, int y, int z) rhs)
+    {
+        return (lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+    }
+
+    public static long mdist((int x, int y, int z) lhs, (int x, int y, int z) rhs)
+    {
+        var v = m.vsub(lhs, rhs);
+        return Math.Abs(v.x) + Math.Abs(v.y) + Math.Abs(v.z);
+    }
+
+    public static (int x, int y, int z) matmul(int[,] t, (int x, int y, int z) p)
+    {
+        return (t[0, 0] * p.x + t[0, 1] * p.y + t[0, 2] * p.z,
+                t[1, 0] * p.x + t[1, 1] * p.y + t[1, 2] * p.z,
+                t[2, 0] * p.x + t[2, 1] * p.y + t[2, 2] * p.z);
+    }
+
+    public static int[,] transpose(int[,] m)
+    {
+        var r = new int[3, 3];
+        for (var i = 0; i < 3; i++)
+        {
+            for (var j = 0; j < 3; j++)
+            {
+                r[i, j] = m[j, i];
+            }
+        }
+        return r;
+    }
 }
